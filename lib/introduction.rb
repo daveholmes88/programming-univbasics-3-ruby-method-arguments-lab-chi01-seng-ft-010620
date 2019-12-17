6d90fc5062a0fa3edc8b35fd2c8c83384047ad84
David Holmes <phoenix1988@gmail.com>
2019-12-17 03:24:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,13 +2,13 @@ def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
-def introduction_with_language(name, language)
-  puts, "Hi, my name is #{name} and I'm learning to program in #{language}"
-end
+# def introduction_with_language(name, language)
+#   puts, "Hi, my name is #{name} and I'm learning to program in #{language}"
+# end
 
-def introduction_with_language_optional(name, language = 'ruby')
-  puts, "Hi, my name is #{name} and I'm learning to program in #{language}"
-end
+# def introduction_with_language_optional(name, language = 'ruby')
+#  puts, "Hi, my name is #{name} and I'm learning to program in #{language}"
+# end
 
 
 
