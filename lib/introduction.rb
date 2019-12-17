00c2a13d1829d2595290b05423672756ab3e0979
David Holmes <phoenix1988@gmail.com>
2019-12-17 03:19:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 def introcudtion(name)
   puts "Hi, my name is #{name}"
-end 
+end
 
 def introduction_with_language(name, language)
   puts, "Hi, my name is #{name} and I'm learning to program in #{language}"
